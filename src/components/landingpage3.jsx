e540e3bda9f25da700050c8770b2018d9b9b93b9
santoshallu1234 <santoshallu1234@gmail.com>
2024-08-29 20:24:50 +0530

    start work

---
+++ src/components/landingpage3.jsx
@@ -0,0 +1,17 @@
+export default function Landingpage3() {
+    return (
+      <>
+      <div className=" w-[100%] h-[100vh]  p-11 page3 pt-0">
+          <div className="inpage3 w-[100%] h-[100%]  flex   rounded-2xl relative overflow-hidden p-8 pl-14 bg-white">
+             <div className="text-white " >
+                <h3>SECTORS</h3>
+                <h2>Our Services</h2>
+                <p>We bring our expertise to multiple solar PV system services, developing the industry's future and redefining what is possible.</p>
+             </div>
+             <div></div>
+          </div>
+      </div>
+      </>
+    );
+  }
+  
