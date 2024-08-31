a5b89f41aff5010055eeded339e51bf8f3102481
santoshallu1234 <santoshallu1234@gmail.com>
2024-08-31 22:42:44 +0530

    new

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -1,7 +1,47 @@
 import Image from "next/image";
-import aboutnew from "../assets/aboutnew.jpg"
+import aboutnew from "../assets/aboutnew.jpg";
+
+
 export default function Landingpage() {
+    const features = [
+        {
+          src: "/images/temp2back.png",
+          title: "SalahCar",
+          src2:"/images/tem2.png",
+          desc:"Unleashing Your Automotive Adventure",
+          link:"https://thesalahcar.com/"
+        },
+       
+        {
+          src: "/images/temp5back.png",
+          title: "Revol-Tail",
+          src2:"/images/tem5.png",
+          desc:"Featured Website Design Projects Custom B2C, B2B and eCommerce solutions",
+          link:"https://silaaiwale.netlify.app/"
+        },
+       
+        {
+          src: "/images/projects/temp7back.png",
+          title: "Sangeet",
+          src2:"/images/projects/sangeet.png",
+          desc:"Featured Website Design Projects Custom B2C, B2B and eCommerce solutions",
+          link:"https://websangeet.netlify.app/"
+        },
+        {
+          src: "/images/projects/girlcomputer.png",
+          title: "Achivemap",
+          src2:"/images/projects/tem4.png",
+          desc:"Featured Website Design Projects Custom B2C, B2B and eCommerce solutions",
+          link:"https://achievemap.vercel.app"
+        },
+      ];
+    
+
+
+
   return (
+    
+
     <>
     <div className=" w-[100%] h-[100vh] landingpage p-11 ">
         <div className="w-[100%] h-[100%]  flex flex-col justify-center rounded-2xl relative overflow-hidden p-11 pl-14 indiv">
